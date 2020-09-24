b0d82639354c8d14fb3450abe768bd08e57dee84
Denis <denis.tojaga@edu.fit.ba>
2020-09-24 17:07:37 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+  /* var MenuItems = document.getElementById("MenuItems");
+
+    MenuItems.style.maxHeight = "0px";
+   document.getElementById("menu-icon").onclick=()=>{
+        if(MenuItems.style.maxHeight == "0px"){
+            MenuItems.style.maxHeight = "200px";
+        }else{
+            MenuItems.style.maxHeight = "0px";
+        }
+    }
+
+*/
+
+
+
+var productImg = document.getElementById("productImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = ()=>{
+    productImg.src = smallImg[0].src;
+}
+
+smallImg[1].onclick = ()=>{
+    productImg.src = smallImg[1].src;
+}
+
+smallImg[2].onclick = ()=>{
+    productImg.src = smallImg[2].src;
+}
+
+smallImg[3].onclick = ()=>{
+    productImg.src = smallImg[3].src;
+}
+
+
+
+
+
+document.getElementById("cartButton").onclick = function(){
+    alert("Your product has been successfully added to your cart");
+}
+
+
+
